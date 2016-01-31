b678ab7ce0f0c4835f9df8b9e4d5850d3a29fd45
mirecki <mmirecki@redhat.com>
2016-01-31 10:42:44 +0100

    Hook changes

--- hooks/opm_after_get_caps.py
+++ hooks/opm_after_get_caps.py
@@ -3,14 +3,25 @@
 import hooking
 import json
 
+network_name = "public"
+nic_name = "ens17"
+
 caps = hooking.read_json()
 networks = caps['networks']
+bridges = caps['bridges']
+nics = caps['nics']
 
-# Update networks with the provider managed network
-# An example off adding network "public" to the caps info
+if networks.get(network_name, None):
+    hooking.exit_hook("", return_code=0)
 
-networks['public'] = {
-    "iface": "public",
+"""
+Update networks with the provider managed network
+An example off adding network <network_name> to the caps info (if the network
+does not exist)
+"""
+
+networks[network_name] = {
+    "iface": network_name,
     "addr": "",
     "cfg":
     {
@@ -36,7 +47,92 @@ networks['public'] = {
     "ipv4addrs": [],
     "mtu": "1500",
     "ipv6gateway": "::",
-    "ports": ["ens11"]
+    "ports": [nic_name]
+}
+
+bridges[network_name] = {
+    "addr": "",
+    "cfg": {"BOOTPROTO": "none"},
+    "ipv6addrs": [],
+    "mtu": "1500",
+    "dhcpv4": False,
+    "netmask": "",
+    "dhcpv6": False,
+    "stp": "off",
+    "ipv4addrs": [],
+    "ipv6gateway": "::",
+    "gateway": "",
+    "opts": {
+        "multicast_last_member_count": "2",
+        "vlan_protocol": "0x8100",
+        "hash_elasticity": "4",
+        "multicast_query_response_interval": "1000",
+        "group_fwd_mask": "0x0",
+        "multicast_snooping": "1",
+        "multicast_startup_query_interval": "3125",
+        "hello_timer": "0",
+        "multicast_querier_interval": "25500",
+        "max_age": "2000",
+        "hash_max": "512",
+        "stp_state": "0",
+        "topology_change_detected": "0",
+        "priority": "32768",
+        "multicast_membership_interval": "26000",
+        "root_path_cost": "0",
+        "root_port": "0",
+        "multicast_querier": "0",
+        "multicast_startup_query_count": "2",
+        "nf_call_iptables": "0",
+        "hello_time": "200",
+        "topology_change": "0",
+        "bridge_id": "8000.fe1a4a160153",
+        "topology_change_timer": "0",
+        "ageing_time": "30000",
+        "nf_call_ip6tables": "0",
+        "gc_timer": "0",
+        "root_id": "8000.fe1a4a160153",
+        "nf_call_arptables": "0",
+        "group_addr": "1:80:c2:0:0:0",
+        "multicast_last_member_interval": "100",
+        "default_pvid": "1",
+        "multicast_query_interval": "12500",
+        "multicast_query_use_ifaddr": "0",
+        "tcn_timer": "0",
+        "multicast_router": "1",
+        "vlan_filtering": "0",
+        "forward_delay": "1500"
+    },
+    "ports": [nic_name]
+}
+
+nic = nics.get(nic_name, None)
+
+if nic:
+    nics[nic_name]["ports"] = [nic_name]
+    hooking.write_json(caps)
+    hooking.exit_hook("", return_code=0)
+
+nics[nic_name] = {
+    "addr": "",
+    "cfg": {
+        "BRIDGE": network_name,
+        "IPV6INIT": "no",
+        "MTU": "1500",
+        "NM_CONTROLLED": "no",
+        "BOOTPROTO": "none",
+        "DEVICE": nic_name,
+        "ONBOOT": "yes"
+    },
+    "ipv6addrs": ["fe80::5054:ff:fec8:3205/64"],
+    "mtu": "1500",
+    "dhcpv4": False,
+    "netmask": "",
+    "dhcpv6": False,
+    "ipv4addrs": [],
+    "hwaddr": "52:54:00:c8:32:05",
+    "ipv6gateway": "::",
+    "gateway": "",
+    "speed": 0
 }
 
 hooking.write_json(caps)
--- hooks/opm_migrate_destination.py
+++ hooks/opm_migrate_destination.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
-import traceback
-from xml.dom import minidom
 import hooking
 from vdsm.network.configurators import libvirt
 from vdsm import netinfo
+import opm_common
 
 VNIC_ID_KEY = 'vnic_id'
 PROVIDER_TYPE_KEY = 'provider_type'
@@ -11,64 +10,12 @@ OPENSTACK_NET_PROVIDER_TYPE = 'OPENSTACK_NETWORK'
 
 
 def main():
-
     domxml = hooking.read_domxml()
-
     interfaces = domxml.getElementsByTagName('interface')
-
     for iface in interfaces:
         source = iface.getElementsByTagName('source')[0]
         bridge_name = source.getAttribute('bridge')
-        add_bridge(bridge_name)
-        add_libvirt_network(bridge_name)
-
-
-def add_libvirt_network(bridge_name):
-    net = libvirt.getNetworkDef("public")
-    if not net:
-        net_xml = "<network><name>" + netinfo.LIBVIRT_NET_PREFIX + \
-            bridge_name + \
-            "</name><forward mode='bridge' /><bridge name='" + bridge_name +\
-            "' /></network>"
-        libvirt.createNetwork(net_xml)
-
-
-def add_bridge(bridge_name):
-    if not does_bridge_exist(bridge_name):
-        create_bridge(bridge_name)
-
-
-"""
-Not persistant
-Requires modyfing the following in /etc/sudoers.d/50_vdsm on host
-Cmnd_Alias VDSM_NET = \
-    /sbin/brctl
-vdsm  ALL=(ALL) NOPASSWD: VDSM_LIFECYCLE, VDSM_STORAGE, VDSM_NET
-"""
-
-
-def run(cmd):
-    rc, out, err = hooking.execCmd(cmd, sudo=True)
-    return out
-
-
-def create_bridge(bridge_name):
-    run(['/sbin/brctl', 'addbr', bridge_name])
-    run(['/sbin/brctl', 'addif', bridge_name, "ens11"])
-
-
-def does_bridge_exist(bridge_name):
-    return bridge_name in get_all_bridges()
-
-
-def get_all_bridges():
-    process_output = run(['brctl', 'show'])
-    output_lines = process_output[1:]
-    tokenized_lines = map(str.split, output_lines)
-    bridge_lines = filter(lambda bridge_tokens: len(bridge_tokens) > 1,
-                          tokenized_lines)
-    bridge_names = map(lambda tokens: tokens[0], bridge_lines)
-    return bridge_names
-
+        opm_common.add_bridge(bridge_name)
+        opm_common.add_libvirt_network(bridge_name)
 
 main()

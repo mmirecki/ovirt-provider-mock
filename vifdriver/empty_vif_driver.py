a658e25c6476e3dad1376dbfcef45459dd7c23b7
mirecki <mmirecki@redhat.com>
2016-06-01 18:52:41 +0200

    Empty vif driver

--- vifdriver/empty_vif_driver.py
+++ vifdriver/empty_vif_driver.py
@@ -1,31 +1,64 @@
 from vif_driver import VIFDriver
 
 
-class DefaultVifDriver(VIFDriver):
+class EmptyVifDriver(VIFDriver):
 
-    def after_get_caps(self):
-        pass
+    def after_device_destroy(self, environ, domxml):
+        return domxml
 
-    def after_get_stats(self, path):
-        pass
+    def after_device_create(self, environ, domxml):
+        return domxml
 
-    def before_nic_hotplug(self, path):
-        pass
+    def after_network_setup(self, environ, json_content):
+        return json_content
 
-    def before_nic_unplug(self, path):
-        pass
+    def after_nic_hotplug(self, environ, domxml):
+        return domxml
+
+    def after_nic_unplug(self, environ, domxml):
+        return domxml
+
+    def after_get_caps(self, environ, json_content):
+        return json_content
+
+    def after_get_stats(self, environ, json_content):
+        return json_content
+
+    def after_vm_start(self, environ, domxml):
+        return domxml
+
+    def after_migration_source(self, environ, domxml):
+        return domxml
+
+    def after_migration_destination(self, environ, domxml):
+        return domxml
+
+    def before_get_caps(self, environ, json_content):
+        return json_content
+
+    def before_get_stats(self, environ, json_content):
+        return json_content
+
+    def before_nic_hotplug(self, environ, domxml):
+        return domxml
+
+    def before_nic_unplug(self, environ, domxml):
+        return domxml
 
     def before_device_create(self, environ, domxml):
-        pass
+        return domxml
 
     def before_device_destroy(self, environ, domxml):
-        pass
+        return domxml
 
-    def before_migration_source(self, path):
-        pass
+    def before_migration_source(self, environ, domxml):
+        return domxml
 
-    def before_migration_destination(self, path):
-        pass
+    def before_migration_destination(self, environ, domxml):
+        return domxml
 
-    def before_network_setup(self, path):
-        pass
+    def before_network_setup(self, environ, json_content):
+        return json_content
+
+    def before_vm_start(self, environ, domxml):
+        return domxml
